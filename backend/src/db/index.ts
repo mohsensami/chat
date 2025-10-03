dcc3e1b736264c2d5ce72d028dae778e940b1d0d
mohsensami <mohsensami18727@gmail.com>
2025-10-03 11:04:16 +0330

    drizzle scema user added

---
+++ backend/src/db/index.ts
@@ -0,0 +1,28 @@
+import { Pool } from "pg";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import * as userSchema from "./schema/user";
+
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_NAME ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD
+) {
+  throw new Error("Database credentials missing... ");
+}
+
+const pool = new Pool({
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+const combinedSchemas = {
+  ...userSchema,
+  // Add other schemas here
+};
+export const db: NodePgDatabase<typeof combinedSchemas> = drizzle(pool, {
+  schema: combinedSchemas,
+});
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { signIn, signUp, signOut, getUsers } from "../controller/user";
+// import { privateRoute } from "../middleware/privateRoute";
+
+const userRouter = Router();
+
+userRouter.post("/signup", signUp);
+userRouter.post("/signin", signIn);
+userRouter.post("/signout", signOut);
+// userRouter.get("/", privateRoute, getUsers);
+
+export default userRouter;
