53eff97ae594169b22dd6310950cac6a577968da
mohsensami <mohsensami18727@gmail.com>
2025-10-11 19:50:43 +0330

    install and config frontend, tailwind, daisy ui

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import userRouter from "./user";
-// import messageRouter from "./message";
+import messageRouter from "./message";
 
 const createRouter = () => {
   const router = Router();
   router.use("/user", userRouter);
-  //   router.use("/message", messageRouter);
+  router.use("/message", messageRouter);
 
   return router;
 };
