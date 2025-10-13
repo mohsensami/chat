d6b264bfbf98a131b68f126489e3a3b3767f70c1
mohsensami <mohsensami18727@gmail.com>
2025-10-13 22:38:17 +0330

    Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,90 @@
+// import { HttpMethod } from "src/helpers/constants";
+import { useForm, type SubmitHandler } from "react-hook-form";
+// import useMutation from "src/hooks/useMutation";
+
+// import { useEffect } from "react";
+// import { useUserContext } from "src/contexts/user";
+import { useNavigate } from "react-router-dom";
+import type { User } from "../types";
+import Input from "../components/SimpleInput";
+
+const Login = () => {
+  // const { execute, data } = useMutation<User>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<User>();
+  // const { setUser } = useUserContext();
+  // const navigate = useNavigate();
+
+  const onGoToRegister = () => {
+    // navigate("/register");
+  };
+
+  const onSubmit: SubmitHandler<User> = (data: User) => {
+    // execute({ url: `user/signin`, body: data, method: HttpMethod.POST });
+  };
+
+  // useEffect(() => {
+  //   if (data) {
+  //     localStorage.setItem("user", JSON.stringify(data));
+  //     setUser(data);
+  //   }
+  // }, [data]);
+
+  return (
+    <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
+        <h1 className="text-3xl font-semibold text-center text-gray-700">
+          Login To Chat
+        </h1>
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            register={register("email", {
+              required: "Email is Required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Email Is Not Valid",
+              },
+            })}
+            error={errors.email}
+            label="Email"
+            name="email"
+            placeholder="Email"
+          />
+
+          <Input
+            register={register("password", {
+              required: "Password is Required",
+              minLength: {
+                value: 6,
+                message: "Password Should have at least 6 characters",
+              },
+            })}
+            error={errors.password}
+            label="Password"
+            name="password"
+            placeholder="Password"
+            type="password"
+          />
+          <div className="flex items-center">
+            <p>Do Not Have An Account?</p>
+            <button className="btn btn-link" onClick={onGoToRegister}>
+              Register
+            </button>
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Login"
+              className="btn btn-block btn-primary text-white"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,4 @@
+import { User, NewUser } from "../../../backend/src/db/schema/user";
+import { Message, NewMessage } from "../../../backend/src/db/schema/message";
+
+export type { User, NewUser, Message, NewMessage };
